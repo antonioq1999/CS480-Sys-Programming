0d87425d2519de406bd10c6007cf7c2d6702e136
Antonio  Quintanilla <74281141+antonioq1999@users.noreply.github.com>
2023-02-05 14:06:09 -0800

    Add files via upload
    Initial commit

---
+++ CS480-Sys-Programming/Assignments/Programming Assignment 1/progassn1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    FILE *in_file = fopen("Hey Boy! Hey Boy!.txt", "r"); // read only
+    FILE *out_file = fopen("lyric_out.txt", "w");        // write to only
+
+    char c;
+
+    do
+    {
+        c = getc(in_file);
+        putc(c, out_file);
+    } while (c != EOF);
+
+    fclose(in_file);
+    fclose(out_file);
+}
