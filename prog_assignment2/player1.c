543753431e426e3b74ac26edabcb44a27b2a0f34
Naotoshi Yoshida <ney2@humboldt.edu>
2023-02-24 12:57:11 -0800

    Progress for A#2
    got pipes to work

--- prog_assignment2/player1.c
+++ prog_assignment2/player1.c
@@ -11,22 +11,41 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 int main()
 {
-    FILE *fplayer1;
-    char buffer[BUFSIZ + 1];
-    int chars_read;
-    char input[60];
-    int res = mkfifo("/tmp/my_fifo", 0777);
+    int fd; 
 
-    if (res == 0)
-        printf("FIFO created\n");
-    memset(buffer, '\0', sizeof(buffer));
-    printf("Enter a string of 5's and 7's: ");
-    fplayer1 = popen("od -c", "w");
+    char *myfifo = "/tmp/myfifo";
 
-    printf("Your string was: %s", fplayer1);
+    mkfifo(myfifo, 0666); 
 
+    char arr1[11], arr2[11]; 
+    while(1){
+
+        fd = open(myfifo, O_WRONLY);
+        printf("Please enter a string of 5s and 7s. limit 10\n"); 
+        fgetc(arr2, 11, stdin); 
+
+        // ASCII value for int 5 and string 5 do not compare
+        for(int i=0; i<strlen(arr2); i++){
+            if(arr2[i] != "5" || arr2[i] != "7"){
+                printf("Please enter a valid string of 5s and 7s.\n"); 
+            }
+        }
+        write(fd, arr2, strlen(arr2)+1); 
+        close(fd);
+        sleep(5); 
+        fd = open(myfifo, O_RDONLY); 
+        read(fd, arr1, sizeof(arr1));
+
+        printf("User2: %s\n", arr1); 
+        close(fd);
+    }
+
+    printf("Enter a String of 5's and 7's:\n");
+    printf("%s", arr2);
+    scanf("%s", arr2); 
     return 0;
 }
